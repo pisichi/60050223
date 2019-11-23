c8de0abd77fa845c1ce4ef9fdaaf90b0af1ec2f6
Wake me up inside <36178784+pisichi@users.noreply.github.com>
2019-11-23 16:26:30 +0700

    hello world

---
+++ 60050223/src/th/co/cdgs/WorkshopApplication.java
@@ -0,0 +1,9 @@
+package th.co.cdgs;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class WorkshopApplication extends Application{
+
+}
